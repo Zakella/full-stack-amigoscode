12895c230e8addeda33458cd69b292a9a261feea
Zakella <zapolski.slava@gmail.com>
2023-05-05 16:32:17 +0300

    changed Customer

--- backend/src/main/java/com/zakella/customer/Customer.java
+++ backend/src/main/java/com/zakella/customer/Customer.java
@@ -1,5 +1,7 @@
 package com.zakella.customer;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
@@ -11,81 +13,19 @@ import java.util.Objects;
                 columnNames = "email")
 
 })
+
+
+@Data
+@NoArgsConstructor
+
 public class Customer {
-    public Customer(String name, String email, Integer age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
 
-    public Customer(Integer id, String name, String email, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
     @Id
-//    @SequenceGenerator(
-//            name = "customer_id_sequence",
-//            sequenceName = "customer_id_sequence"
-//    )
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
+    private Integer id;
+    private String name;
+    private String email;
+    private String age;
+    private Gender gender;
 
-    @Column(nullable = false)
-    String name;
 
-    @Column(nullable = false)
-    String email;
-
-    @Column(nullable = false)
-    Integer age;
-
-    public Customer() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, age);
-    }
 }
