5a62782c5102614bae901d29908e2f81277f5915
Zakella <zapolski.slava@gmail.com>
2023-05-25 16:29:31 +0300

    angular 592

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -1,23 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MyFirstComponent } from './my-first-component/my-first.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import { CustomerComponent } from './components/customer/customer.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import {AvatarModule} from "primeng/avatar";
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponent
+    CustomerComponent,
+    MenuBarComponent,
+    MenuItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    InputTextModule,
+    AvatarModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
